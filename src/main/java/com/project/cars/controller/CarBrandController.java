cb3339fcd61c8defb450b1eb60f061d9bda40f59
Victor Terroso <v.terroso87@gmail.com>
2024-01-09 18:05:39 +0100

    some corrections

--- src/main/java/com/project/cars/controller/CarBrandController.java
+++ src/main/java/com/project/cars/controller/CarBrandController.java
@@ -25,7 +25,7 @@ public class CarBrandController {
      * This method is used to get all the cars brands.
      * @return a list of CarsBrandPojo objects.
      */
-    @GetMapping("/car-brand/all")
+    @GetMapping()
     public ResponseEntity<List<CarBrandPojo>> getAll() {
         return ResponseEntity.status(HttpStatus.OK)
                 .body(iCarBrandService.getAll());
@@ -34,18 +34,18 @@ public class CarBrandController {
     /**
      * This method is used to get a cars brand by its id.
      * @param id is the id of the cars brand to be searched.
-     * @return a CarsBrandPojo object.
+     * @return a CarsBrandPojo object
      */
-    @GetMapping("/car-brand/{id}")
-    public ResponseEntity<CarBrandPojo> getCarsBrand(Long id) {
+    @GetMapping(path="/{id}")
+    public ResponseEntity<CarBrandPojo> getCarsBrand(@PathVariable Long id) {
         return ResponseEntity.of(iCarBrandService.getCarsBrand(id));
     }
     /**
      * This method is used to save a cars brand.
      * @param newCarsBrandPojo is the CarsBrandPojo object to be saved.
-     * @return a CarsBrandPojo object.
+     * @return a Bad Request response if the object is not valid or a CarsBrandPojo object.
      */
-    @PostMapping("/car-brand/save")
+    @PostMapping()
     public ResponseEntity<CarBrandPojo> save(@RequestBody CarBrandPojo newCarsBrandPojo) {
         try {
             return ResponseEntity.status(HttpStatus.CREATED)
@@ -54,5 +54,30 @@ public class CarBrandController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                     .build();
         }
+    }
+    /**
+     * This method is used to update a cars brand.
+     * @param newCarsBrandPojo is the CarsBrandPojo object to be updated.
+     * @return a CarsBrandPojo object.
+     */
+    @PutMapping()
+    public ResponseEntity<CarBrandPojo> update(@RequestBody CarBrandPojo newCarsBrandPojo) {
+        try {
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body(iCarBrandService.save(newCarsBrandPojo));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .build();
+        }
+    }
+
+    /**
+     * This method is used to delete a cars brand by its id.
+     * @param id is the id of the cars brand to be deleted.
+     * @return a boolean value.
+     */
+    @DeleteMapping(path="/{id}")
+    public ResponseEntity<Boolean> delete(@PathVariable Long id) {
+        return new ResponseEntity<>(iCarBrandService.delete(id) ? HttpStatus.OK : HttpStatus.NOT_FOUND);
     }
 }
